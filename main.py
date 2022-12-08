6a8f027474acd33b5db1da57ca9033ad82ee47af
AdrianSzklarski <adrian_it@o2.pl>
2022-12-09 00:37:17 +0100

    Added acceptance of cookies & drop-down menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # imports for scrapping to olx page
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import tkinter as tk
@@ -17,6 +18,7 @@ class OlxPage:
         # Calling up methods
         self.get_window()
         self.get_dir()
+        self.get_driver()
 
     def get_window(self):
         self.root.title('Porsche Cars')
@@ -26,8 +28,20 @@ class OlxPage:
         save_dir = os.path.dirname(__file__)
         os.chdir(save_dir)
 
+    def get_driver(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.option)
+        driver.get('https://www.olx.pl/d/motoryzacja/samochody/porsche/')
+        driver.maximize_window()
+        driver.find_element(By.ID, 'onetrust-accept-btn-handler').click()
+        driver.find_element(By.CLASS_NAME, 'css-mf5jvh').click()
+        time.sleep(2)
+        print(driver.find_element(By.CLASS_NAME, 'css-1ee1qo5').click())
+        time.sleep(2)
+
 
 if __name__ == '__main__':
     root = tk.Tk()
     app = OlxPage(root)
     root.mainloop()
+
+
