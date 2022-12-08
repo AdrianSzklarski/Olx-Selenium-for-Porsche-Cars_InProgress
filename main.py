1c2a90d22c6139899255b63bde8adb159b7701f2
AdrianSzklarski <adrian_it@o2.pl>
2022-12-08 22:29:29 +0100

    Added controll of interface

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 # imports for scrapping to olx page
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import tkinter as tk
 import time, os
+
 
 class OlxPage:
     def __init__(self, root):
         self.root = root
+        self.option = Options()
+
+        # controll of interface of user
+        self.option.headless = False
+
+
+
 
 
 if __name__ == '__main__':
