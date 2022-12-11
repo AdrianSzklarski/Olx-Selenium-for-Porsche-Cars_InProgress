d5ceba81a980dc930c159db37aced41ba04b77b3
AdrianSzklarski <adrian_it@o2.pl>
2022-12-11 19:48:59 +0100

    Comments added

--- main.py
+++ main.py
@@ -21,14 +21,17 @@ class OlxPage:
         self.get_selection_model()
 
     def get_window(self):
+        ''' Main window settings '''
         self.root.title('Porsche Cars')
         self.root.wm_attributes('-zoomed', True)
 
     def get_dir(self):
+        ''' Setting the root directory '''
         save_dir = os.path.dirname(__file__)
         os.chdir(save_dir)
 
     def get_selection_model(self):
+        ''' Addition of car model selection buttons for analysis '''
         tk.Label(self.root, text="Select of model's Porsche: ").place(x=60, y=60)
 
         self.radioValue = tk.IntVar(self.root, 0)
@@ -43,14 +46,14 @@ class OlxPage:
         tk.Radiobutton(self.root, text="More", variable=self.radioValue, value=9, command=self.get_driver).place(x=50, y=420)
 
     def get_driver(self):
+        ''' Scrapping the olx page for Porsche '''
         driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.option)
         driver.get('https://www.olx.pl/d/motoryzacja/samochody/porsche/')
         driver.maximize_window()
         driver.find_element(By.ID, 'onetrust-accept-btn-handler').click()
         driver.find_element(By.CLASS_NAME, 'css-mf5jvh').click()
 
-        print(driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/form/div[3]/div[1]/div/div[3]/div/div/div[2]/div/div[' + str(self.radioValue.get()) + ']/label/input').click(), time.sleep(8))
-
+        driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/form/div[3]/div[1]/div/div[3]/div/div/div[2]/div/div[' + str(self.radioValue.get()) + ']/label/input').click()
 
 if __name__ == '__main__':
     root = tk.Tk()
