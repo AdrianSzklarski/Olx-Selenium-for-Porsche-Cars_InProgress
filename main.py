dcc5ef595cf0baf7de0579932be9b11d1897b5be
AdrianSzklarski <adrian_it@o2.pl>
2022-12-11 19:33:15 +0100

    Added select button of the models cars

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ class OlxPage:
         self.get_window()
         self.get_dir()
         self.get_selection_model()
-        self.get_driver()
 
     def get_window(self):
         self.root.title('Porsche Cars')
@@ -32,60 +31,28 @@ class OlxPage:
     def get_selection_model(self):
         tk.Label(self.root, text="Select of model's Porsche: ").place(x=60, y=60)
 
-        tk.Radiobutton(self.root,
-                       text="Cayenne",
-                       variable=tk.IntVar(),
-                       value=1).place(x=50, y=100)
-
-        tk.Radiobutton(self.root,
-                       text="911",
-                       variable=tk.IntVar(),
-                       value=2).place(x=50, y=120)
-
-        tk.Radiobutton(self.root,
-                       text="Cayenne S",
-                       variable=tk.IntVar(),
-                       value=3).place(x=50, y=140)
-
-        tk.Radiobutton(self.root,
-                       text="Panamera",
-                       variable=tk.IntVar(),
-                       value=4).place(x=50, y=160)
-
-        tk.Radiobutton(self.root,
-                       text="Boxter",
-                       variable=tk.IntVar(),
-                       value=5).place(x=50, y=180)
-
-        tk.Radiobutton(self.root,
-                       text="944",
-                       variable=tk.IntVar(),
-                       value=6).place(x=50, y=200)
-
-        tk.Radiobutton(self.root,
-                       text="Cayenne Turbo",
-                       variable=tk.IntVar(),
-                       value=7).place(x=50, y=220)
-
-        tk.Radiobutton(self.root,
-                       text="More",
-                       variable=tk.IntVar(),
-                       value=8).place(x=50, y=240)
-
+        self.radioValue = tk.IntVar(self.root, 0)
+        tk.Radiobutton(self.root, text="All", variable=self.radioValue, value=1, command=self.get_driver).place(x=50, y=100)
+        tk.Radiobutton(self.root, text="Cayenne", variable=self.radioValue, value=2, command=self.get_driver).place(x=50, y=140)
+        tk.Radiobutton(self.root, text="911", variable=self.radioValue, value=3, command=self.get_driver).place(x=50, y=180)
+        tk.Radiobutton(self.root, text="Cayenne S", variable=self.radioValue, value=4, command=self.get_driver).place(x=50, y=220)
+        tk.Radiobutton(self.root, text="Panamera", variable=self.radioValue, value=5, command=self.get_driver).place(x=50, y=260)
+        tk.Radiobutton(self.root, text="Boxter", variable=self.radioValue, value=6, command=self.get_driver).place(x=50, y=300)
+        tk.Radiobutton(self.root, text="944", variable=self.radioValue, value=7, command=self.get_driver).place(x=50, y=340)
+        tk.Radiobutton(self.root, text="Cayenne Turbo", variable=self.radioValue, value=8, command=self.get_driver).place(x=50, y=380)
+        tk.Radiobutton(self.root, text="More", variable=self.radioValue, value=9, command=self.get_driver).place(x=50, y=420)
 
     def get_driver(self):
-        test = 5
         driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.option)
         driver.get('https://www.olx.pl/d/motoryzacja/samochody/porsche/')
         driver.maximize_window()
         driver.find_element(By.ID, 'onetrust-accept-btn-handler').click()
         driver.find_element(By.CLASS_NAME, 'css-mf5jvh').click()
-        driver.find_element(By.XPATH,
-                            '//*[@id="root"]/div[1]/div[2]/form/div[3]/div[1]/div/div[3]/div/div/div[2]/div/div[' + str(test) + ']/label/input').click()
+
+        print(driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/form/div[3]/div[1]/div/div[3]/div/div/div[2]/div/div[' + str(self.radioValue.get()) + ']/label/input').click(), time.sleep(8))
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     app = OlxPage(root)
     root.mainloop()
-
