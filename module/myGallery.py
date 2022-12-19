2b91dc2e68f0ee44a9180d6abf1ca598f9c75bd5
AdrianSzklarski <adrian_it@o2.pl>
2022-12-20 00:37:20 +0100

    Added photo test module

--- module/myGallery.py
+++ module/myGallery.py
@@ -1,12 +1,30 @@
-# import tkinter as tk
+import tkinter as tk
+from PIL import Image, ImageTk
+
+
 class MyGalleryOfCars:
     def __init__(self, root):
         self.root = root
         self.root.config(width=300, height=200)
         self.root.title("Secondary Window")
         self.root.focus()
+        self.get_read_photo()
 
         self.root.mainloop()
+
+    def get_read_photo(self):
+        link = r'/home/adrian/Pulpit/selenium_olx/Test_photo.png'
+        load = Image.open(link)
+        image = load.resize((600, 400), Image.ANTIALIAS)
+        image.save(fp='/home/adrian/Pulpit/selenium_olx/Test_photo_scale.png')
+
+        link = r'/home/adrian/Pulpit/selenium_olx/Test_photo_scale.png'
+        load = Image.open(link)
+        render = ImageTk.PhotoImage(load)
+        img = tk.Label(self.root, image=render)
+        img.image = render
+        img.place(x=300, y=50)
+
 
     def get_next(self):
         print('Next')
